f46ff288b7bf80a32a8c517d4c6f7468f8be8a2a
Elias Christiansen <69719887+EliasIXDesign@users.noreply.github.com>
2022-04-25 08:43:34 +0200

    WorkingBarely.ino

--- CamerShutterSerial.ino
+++ CamerShutterSerial.ino
@@ -9,15 +9,13 @@ CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS  255
 
-int x;
 const int buttonPin = 2;
 
 int buttonState = 0; 
 
-
 void setup() {
  Serial.begin(115200);
- Serial.setTimeout(1);
+ //Serial.setTimeout(1);
 
  pinMode(buttonPin, INPUT);
  
@@ -33,9 +31,11 @@ void loop() {
   leds[n] = CRGB::White ;
   FastLED.show();
    }
-  while (!Serial.available());
-  x = Serial.readString().toInt();
-  Serial.print(x + 1);
+  //while (!Serial.available());
+  delay ( 1000 );
+  Serial.print("1");
+   
+  
 
   for(int n = 0; n < NUM_LEDS; n++) {
   leds[n] = CRGB::Black ;
